1f17c9c7b7e85ed8c2b4e51a80e7015ddc2d5117
Abdelrahman <abdelrahman.hammam95@gmail.com>
2018-04-14 16:30:09 +0200

    Init the project

---
+++ server.py
@@ -0,0 +1,63 @@
+import socket, sys
+import threading
+import time
+import mraa
+ 
+TCP_IP = '192.168.3.2' # your ip
+ 
+TCP_PORTS = [5001,5002,5003,5004,5005,5006,5007,5008]
+ 
+BUFFER_SIZE = 1024
+ 
+ 
+class listen_to_port(threading.Thread):
+ 
+    def __init__(self,thread_name,ip,port_number,buffer_size):
+                threading.Thread.__init__(self)
+                self.thread_name=thread_name
+                self.ip=ip
+                self.port_number=port_number
+                self.buffer_size=buffer_size
+ 
+    def run(self):
+        print("opening thread: "+self.thread_name)
+ 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+        s.bind((self.ip, self.port_number))
+ 
+        s.listen(1)
+ 
+        while 1:
+                conn, addr = s.accept()
+ 
+                print ('Connection address:', addr)
+                gpio_1 = mraa.Gpio(7)
+                gpio_1.dir(mraa.DIR_OUT)
+                current_status = 0
+ 
+                while 1:
+ 
+                        data = conn.recv(self.buffer_size)
+ 
+                        if data.find("on") != -1:
+          	        current_status = 1
+                        else:
+                                current_status = 0
+                        gpio_1.write(current_status)
+                        print ("received data:", data)
+ 
+                        if data == 'close':
+                                break
+                conn.close()
+        s.close()
+        s = none
+ 
+ 
+for port_number in TCP_PORTS:
+    try:
+        thread =listen_to_port("Thread"+str(port_number),TCP_IP,port_number,BUFFER_SIZE)
+        thread.start()
+    except:
+        print("problem in starting thread")
+
